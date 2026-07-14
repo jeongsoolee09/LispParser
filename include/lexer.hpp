62e633ff71474ddd96cb542cfb5ce25367222125
jeongsoolee09 <jeongsoolee09@github.com>
2026-07-13 20:49:15 -0400

    Add draft (Thanks @MichaelRFairhurst!)

---
+++ include/lexer.hpp
@@ -0,0 +1,50 @@
+#include <string>
+#include <string_view>
+#include <variant>
+#include <vector>
+
+enum class Keyword { LPAREN, RPAREN, QUOTE, ATOM, EQ, CAR, CDR, CONS, COND };
+
+struct Symbol {
+  std::string value;
+};
+
+using LispToken = std::variant<Keyword, Symbol>;
+
+// v inline is a suggestion
+// Keyword is essentially an integer and it's not worth the
+// borrow
+inline std::string to_string(Keyword token) {
+  switch (token) {
+  case Keyword::LPAREN:
+    return "LPAREN";
+  case Keyword::RPAREN:
+    return "RPAREN";
+  case Keyword::QUOTE:
+    return "QUOTE";
+  case Keyword::ATOM:
+    return "ATOM";
+  case Keyword::EQ:
+    return "EQ";
+  case Keyword::CAR:
+    return "CAR";
+  case Keyword::CDR:
+    return "CDR";
+  case Keyword::CONS:
+    return "CONS";
+  case Keyword::COND:
+    return "COND";
+  }
+}
+
+inline std::string to_string(const Symbol &symbol) { return symbol.value; }
+
+inline std::string to_string(LispToken token) {
+  return std::visit(
+      [](auto &actual_token) {
+        return to_string(actual_token);
+      }, // passing an inline lambda to a template
+      token);
+}
+
+std::vector<LispToken> lex(std::string_view source);
---
+++ src/lexer.cpp
@@ -0,0 +1,95 @@
+#include "lexer.hpp"
+#include <algorithm>
+#include <cctype>
+#include <iostream>
+#include <iterator>
+
+// struct iterator {
+// 	T* current_item;
+// }
+
+// vectors are contiguous piece of memory.
+// when it grows, it may get realloced to a different location
+
+// If the input is not null-terminated, then it would cause an
+// undefined behavior if that happen to start with one of the
+// characters listed in the switch case.
+// e.g. "q".
+std::vector<LispToken> lex(std::string_view source) {
+  std::vector<LispToken> result;
+  auto it = source.cbegin();
+  for (; it != source.cend(); it++) {
+    char character = *it;
+    switch (character) {
+    case '(':
+      result.emplace_back(Keyword::LPAREN);
+      break;
+    case ')':
+      result.emplace_back(Keyword::RPAREN);
+      break;
+    case 'q': // We might be in "quote"
+      if (*(it + 1) == 'u' && *(it + 2) == 'o' && *(it + 3) == 't' &&
+          *(it + 4) == 'e') {
+        result.emplace_back(Keyword::QUOTE);
+        it += 4;
+        break;
+      }
+    case 'c': // We might be in "car" / "cdr" / "cons" / "cond"
+      if (*(it + 1) == 'a' && *(it + 2) == 'r') {
+        result.emplace_back(Keyword::CAR);
+        it += 2;
+        break;
+      }
+
+      if (*(it + 1) == 'd' && *(it + 2) == 'r') {
+        result.emplace_back(Keyword::CDR);
+        it += 2;
+        break;
+      }
+
+      if (*(it + 1) == 'o' && *(it + 2) == 'n') {
+        if (*(it + 3) == 's') {
+          result.emplace_back(Keyword::CONS);
+          it += 3;
+          break;
+        }
+        if (*(it + 3) == 'd') {
+          result.emplace_back(Keyword::COND);
+          it += 3;
+          break;
+        }
+      }
+    case 'e':
+      if (*(it + 1) == 'q') {
+        result.emplace_back(Keyword::EQ);
+        it += 1;
+        break;
+      }
+    case ' ':
+    case '\n':
+    case '\t':
+      continue; // ignore all whitespaces
+    default:    // very likely a symbol
+      auto occurrence = std::find_if(it, source.cend(), [](char character) {
+        return !std::isalnum(character);
+      });
+      std::ptrdiff_t distance = std::distance(it, occurrence);
+      std::cout << "current_char: " << *it
+                << ", occurring char: " << *occurrence
+                << ", distance: " << distance << "\n";
+      std::string value(&*it, distance);
+      result.emplace_back(Symbol(value));
+      it += distance - 1;
+      break;
+    }
+  }
+  return result;
+}
+
+// No overlaps
+// q -> u ->
+
+// Overlapping chars
+// c -> a -> r
+//   -> d -> r
+// ...

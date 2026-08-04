b00aa67c4e421258231ff06693de78b1af669114
jeongsoolee09 <jeongsoolee09@github.com>
2026-08-04 14:39:36 -0400

    Update tests

--- test/lex/test_lexer.cpp
+++ test/lex/test_lexer.cpp
@@ -13,61 +13,83 @@ TEST_F(LexerTest, EmptyInput) {
 
 TEST_F(LexerTest, EmptyList) {
   std::vector<LispToken> result = lex("()");
-  auto expected_result =
-      std::vector<LispToken>({Delimiter::LPAREN, Delimiter::RPAREN});
+  auto expected_result = std::vector<LispToken>({
+      LParen{ Range{ 1, 1, 1, 2 } },
+      RParen{ Range{ 1, 2, 1, 3 } },
+  });
   EXPECT_EQ(result, expected_result);
 }
 
 TEST_F(LexerTest, NonAlnumSingleChar) {
   std::vector<LispToken> result = lex("!");
-  std::vector<LispToken> expected_result{UserSymbol("!")};
+  std::vector<LispToken> expected_result{ UserSymbol{ "!",
+                                                      Range{ 1, 1, 1, 2 } } };
   EXPECT_EQ(result, expected_result);
 }
 
 TEST_F(LexerTest, Quote) {
   std::vector<LispToken> result = lex("(quote car)");
   auto expected_result =
-      std::vector<LispToken>{Delimiter::LPAREN, KeywordSymbol::QUOTE,
-                             KeywordSymbol::CAR, Delimiter::RPAREN};
+      std::vector<LispToken>{ LParen{ Range{ 1, 1, 1, 2 } },
+                              QuoteKeywordSymbol{
+                                  Range{ 1, 2, 1, 7 },
+                              },
+                              CarKeywordSymbol{ Range{ 1, 8, 1, 11 } },
+                              RParen{ Range{ 1, 11, 1, 12 } } };
   EXPECT_EQ(result, expected_result);
 }
 
 TEST_F(LexerTest, Cond) {
   std::vector<LispToken> result = lex("(cond a b)");
-  auto expected_result = std::vector<LispToken>{
-      Delimiter::LPAREN, KeywordSymbol::COND, UserSymbol("a"), UserSymbol("b"),
-      Delimiter::RPAREN};
+  auto expected_result =
+      std::vector<LispToken>{ LParen{ Range{ 1, 1, 1, 2 } },
+                              CondKeywordSymbol{ Range{ 1, 2, 1, 6 } },
+                              UserSymbol{ "a", Range{ 1, 7, 1, 8 } },
+                              UserSymbol{ "b", Range{ 1, 9, 1, 10 } },
+                              RParen{ Range{ 1, 10, 1, 11 } } };
   EXPECT_EQ(result, expected_result);
 }
 
 TEST_F(LexerTest, Car) {
   std::vector<LispToken> result = lex("(car a b)");
-  auto expected_result = std::vector<LispToken>{
-      Delimiter::LPAREN, KeywordSymbol::CAR, UserSymbol("a"), UserSymbol("b"),
-      Delimiter::RPAREN};
+  auto expected_result =
+      std::vector<LispToken>{ LParen{ Range{ 1, 1, 1, 2 } },
+                              CarKeywordSymbol{ Range{ 1, 2, 1, 5 } },
+                              UserSymbol{ "a", Range{ 1, 6, 1, 7 } },
+                              UserSymbol{ "b", Range{ 1, 8, 1, 9 } },
+                              RParen{ Range{ 1, 9, 1, 10 } } };
   EXPECT_EQ(result, expected_result);
 }
 
 TEST_F(LexerTest, Cdr) {
   std::vector<LispToken> result = lex("(cdr a b)");
-  auto expected_result = std::vector<LispToken>{
-      Delimiter::LPAREN, KeywordSymbol::CDR, UserSymbol("a"), UserSymbol("b"),
-      Delimiter::RPAREN};
+  auto expected_result =
+      std::vector<LispToken>{ LParen{ Range{ 1, 1, 1, 2 } },
+                              CdrKeywordSymbol{ Range{ 1, 2, 1, 5 } },
+                              UserSymbol{ "a", Range{ 1, 6, 1, 7 } },
+                              UserSymbol{ "b", Range{ 1, 8, 1, 9 } },
+                              RParen{ Range{ 1, 9, 1, 10 } } };
   EXPECT_EQ(result, expected_result);
 }
 
 TEST_F(LexerTest, Cons) {
   std::vector<LispToken> result = lex("(cons a (b c))");
-  auto expected_result = std::vector<LispToken>{
-      Delimiter::LPAREN, KeywordSymbol::CONS, UserSymbol("a"),
-      Delimiter::LPAREN, UserSymbol("b"),     UserSymbol("c"),
-      Delimiter::RPAREN, Delimiter::RPAREN};
+  auto expected_result =
+      std::vector<LispToken>{ LParen{ Range{ 1, 1, 1, 2 } },
+                              ConsKeywordSymbol{ Range{ 1, 2, 1, 6 } },
+                              UserSymbol{ "a", Range{ 1, 7, 1, 8 } },
+                              LParen{ Range{ 1, 9, 1, 10 } },
+                              UserSymbol{ "b", Range{ 1, 10, 1, 11 } },
+                              UserSymbol{ "c", Range{ 1, 12, 1, 13 } },
+                              RParen{ Range{ 1, 13, 1, 14 } },
+                              RParen{ Range{ 1, 14, 1, 15 } } };
   EXPECT_EQ(result, expected_result);
 }
 
 TEST_F(LexerTest, StringLiteral) {
   std::vector<LispToken> result = lex("\"hi\"");
-  auto expected_result = std::vector<LispToken>{StringLiteral("hi")};
+  auto expected_result =
+      std::vector<LispToken>{ StringLiteral{ "hi", Range{ 1, 1, 1, 5 } } };
   EXPECT_EQ(result, expected_result);
 }
 } // namespace
